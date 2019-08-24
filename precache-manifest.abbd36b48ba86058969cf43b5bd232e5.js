6e2384bbe23b74b396f1b358c42f5db1f584e0ce
kelly6414143 <kelly6414143@gmail.com>
2019-08-24 13:00:09 +0800

    Updates

---
+++ precache-manifest.abbd36b48ba86058969cf43b5bd232e5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429225e99ea6bd3029a18982c3a32db0",
+    "url": "/F2E_LEVEL6_HotelReservation/index.html"
+  },
+  {
+    "revision": "47a1f9e7b51058c29174",
+    "url": "/F2E_LEVEL6_HotelReservation/static/css/2.b9146f23.chunk.css"
+  },
+  {
+    "revision": "9d0472c1856e84627fef",
+    "url": "/F2E_LEVEL6_HotelReservation/static/css/main.4e743b2f.chunk.css"
+  },
+  {
+    "revision": "47a1f9e7b51058c29174",
+    "url": "/F2E_LEVEL6_HotelReservation/static/js/2.297949c0.chunk.js"
+  },
+  {
+    "revision": "9d0472c1856e84627fef",
+    "url": "/F2E_LEVEL6_HotelReservation/static/js/main.67ddab36.chunk.js"
+  },
+  {
+    "revision": "3843b9c89840b305d270",
+    "url": "/F2E_LEVEL6_HotelReservation/static/js/runtime~main.aef853f6.js"
+  },
+  {
+    "revision": "6312ea36e705d2bf3c57e33892c10a4e",
+    "url": "/F2E_LEVEL6_HotelReservation/static/media/backgroundImage.6312ea36.jpg"
+  },
+  {
+    "revision": "db939d706ed7667ba3703bf88de58e4c",
+    "url": "/F2E_LEVEL6_HotelReservation/static/media/bedroom.db939d70.svg"
+  },
+  {
+    "revision": "a94319e579d3b91a5b3db98ae922c86e",
+    "url": "/F2E_LEVEL6_HotelReservation/static/media/logo.a94319e5.png"
+  },
+  {
+    "revision": "fa3bd6d67fee9a15ea2e0747e7e633dd",
+    "url": "/F2E_LEVEL6_HotelReservation/static/media/map.fa3bd6d6.svg"
+  },
+  {
+    "revision": "7a95ab0fa15db72c93e01b9076099f80",
+    "url": "/F2E_LEVEL6_HotelReservation/static/media/new.7a95ab0f.svg"
+  },
+  {
+    "revision": "21ec2ace26cf98dd47085db975a3c74d",
+    "url": "/F2E_LEVEL6_HotelReservation/static/media/person.21ec2ace.svg"
+  },
+  {
+    "revision": "602bb3ab832b3316719f4b6b17b2689f",
+    "url": "/F2E_LEVEL6_HotelReservation/static/media/room_service.602bb3ab.svg"
+  }
+]);
