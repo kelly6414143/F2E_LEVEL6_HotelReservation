d0dadff118b45bcd9b82701188790ac5b6a974cb
kelly6414143 <kelly6414143@gmail.com>
2019-08-24 13:31:05 +0800

    改router

--- react-hotel-reservation/src/App.js
+++ react-hotel-reservation/src/App.js
@@ -4,12 +4,19 @@ import Home from './components/home';
 import Footer from './components/footer';
 import InnerPage from './components/innerPage'
 import reservationDetail from './components/reservationDetail'
+import createHistory from 'history/createHashHistory'
+
+const history = createHistory({
+
+    basename: 'homepage'
+
+  })
 
 // import { Button } from 'reactstrap';
 
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <div className="App">
         <Route exact path='/' component={Home}/>
         <Route path='/innerPage/:id' component={InnerPage}/>
