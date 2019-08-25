7f3a2edaff96c6f4b894cf176253e695cc552331
kelly6414143 <kelly6414143@gmail.com>
2019-08-26 01:30:17 +0800

    完善驗證綁定

--- react-hotel-reservation/src/components/reservationDetail.js
+++ react-hotel-reservation/src/components/reservationDetail.js
@@ -36,10 +36,15 @@ export default class reservationDetail extends Component {
             fisrtNameinValid:false,
             lastNameValid:false,
             lastNameinValid:false,
+            telValid:false,
+            telinValid:false,
             emailinValid:false,
             emailValid:false,
-            selectedStartDay: undefined,
-            selectedEndDay: undefined,
+            checkEmailinValid:false,
+            checkEmailValid:false,
+            selectedStartDay: this.props.location.state.startDay,
+            selectedEndDay: this.props.location.state.endDay,
+            endDayFirstDate: this.props.location.state.startDay.getTime()+24*60*60*1000,
             isEmpty: true,
             isDisabled: false,
         };
@@ -108,8 +113,11 @@ export default class reservationDetail extends Component {
         });
     }
     getTel(e){
+        let valid = /^[0][9][\d]{8}$/
         this.setState({
-            formTel:e.target.value
+            formTel:e.target.value,
+            telValid:valid.test(e.target.value),
+            telinValid:!valid.test(e.target.value)
         });
     }
     getEmail(e){
@@ -129,9 +137,20 @@ export default class reservationDetail extends Component {
         }
     }
     getCheckEmail(e){
-        this.setState({
-            formCheckEmail:e.target.value
-        });
+        if(e.target.value){
+            let valid = e.target.value===this.state.formEmail?true:false
+            this.setState({
+                formCheckEmail:e.target.value,
+                checkEmailValid:valid,
+                checkEmailinValid:!valid
+            });
+        }else{
+            this.setState({
+                formCheckEmail:e.target.value,
+                checkEmailValid:false,
+                checkEmailinValid:false
+            });
+        }
     }
     getExtraServiceSelect(e){
         this.setState({
@@ -140,48 +159,48 @@ export default class reservationDetail extends Component {
     }
 
     submitReservation(){
-        alert('完善驗證綁定中')
-        // function formatTime(time){
-        //     let formatTime = time.split('/')
-        //     if(formatTime[1]<10){formatTime[1]=`0${formatTime[1]}`}
-        //     if(formatTime[2]<10){formatTime[2]=`0${formatTime[2]}`}
-        //     return formatTime
-        // }
+        // alert('完善驗證綁定中')
+        function formatTime(time){
+            let formatTime = time.split('/')
+            if(formatTime[1]<10){formatTime[1]=`0${formatTime[1]}`}
+            if(formatTime[2]<10){formatTime[2]=`0${formatTime[2]}`}
+            return formatTime
+        }
 
-        // let{
-        //     formSex,
-        //     formFirstname,
-        //     formLastname,
-        //     formTel,
-        //     formEmail,
-        //     formCheckEmail,
-        //     formExtraService,
-        //     selectedStartDay,
-        //     selectedEndDay}=this.state
-        //     selectedStartDay =selectedStartDay?formatTime(selectedStartDay.toLocaleDateString()).join('-') :""
-        //     selectedEndDay = selectedEndDay? formatTime(selectedEndDay.toLocaleDateString()).join('-'):""
-        // let submitData = {
-        //     name: formFirstname + formLastname,
-        //     tel: formTel,
-        //     date: [selectedStartDay,selectedEndDay]
-        // }
-        // // console.log(submitData)
-        // fetch(`https://challenge.thef2e.com/api/thef2e2019/stage6/room/${this.state.roomDetail.id}`,
-        //     {
-        //     method:"POST",
-        //     headers: new Headers({
-        //         'Accept': 'application/json',
-        //         'Content-Type': 'application/json',
-        //         'Authorization': 'Bearer i2A9LrARnW0vSgOGpO9E3X7kDM1tZDQwRLDK1qOlXxXtzVKG6ByZN2JXLxqC',
-        //     }),
-        //     body: JSON.stringify(submitData)
-        // }).then(response =>{
-        //         return response.json(); 
-        //     }).then((jsonData) => {
-        //         // console.log(jsonData);
-        //     }).catch((error)=>{
-        //         console.log(error)
-        //     })
+        let{
+            formSex,
+            formFirstname,
+            formLastname,
+            formTel,
+            formEmail,
+            formCheckEmail,
+            formExtraService,
+            selectedStartDay,
+            selectedEndDay}=this.state
+            selectedStartDay =selectedStartDay?formatTime(selectedStartDay.toLocaleDateString()).join('-') :""
+            selectedEndDay = selectedEndDay? formatTime(selectedEndDay.toLocaleDateString()).join('-'):""
+        let submitData = {
+            name: formFirstname + formLastname,
+            tel: formTel,
+            date: [selectedStartDay,selectedEndDay]
+        }
+        // console.log(submitData)
+        fetch(`https://challenge.thef2e.com/api/thef2e2019/stage6/room/${this.state.roomDetail.id}`,
+            {
+            method:"POST",
+            headers: new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer i2A9LrARnW0vSgOGpO9E3X7kDM1tZDQwRLDK1qOlXxXtzVKG6ByZN2JXLxqC',
+            }),
+            body: JSON.stringify(submitData)
+        }).then(response =>{
+                return response.json(); 
+            }).then((jsonData) => {
+                // console.log(jsonData);
+            }).catch((error)=>{
+                console.log(error)
+            })
     } 
 
       render() {
@@ -233,8 +252,8 @@ export default class reservationDetail extends Component {
 
                             <Form>
                                 <span className="reservationTag">訂房資料</span>
-                                <Row form>
-                                    <Col md={6}>
+                                <Row form className="datePickerRow">
+                                    <Col md={4}>
                                         <Label for="exampleEmail">入住日期</Label>
                                         <FormGroup>
                                             <DayPickerInput
@@ -248,7 +267,7 @@ export default class reservationDetail extends Component {
                                             />
                                         </FormGroup>
                                     </Col>
-                                    <Col md={6}>
+                                    <Col md={4}>
                                         <Label for="exampleEmail">退房日期</Label>
                                         <FormGroup>
                                             <DayPickerInput
@@ -263,20 +282,23 @@ export default class reservationDetail extends Component {
                                             />
                                         </FormGroup>
                                     </Col>
+                                    <Col md={4}>
+                                        歡迎您的蒞臨，誠摯為您服務{(this.state.selectedEndDay.getTime()-this.state.selectedStartDay.getTime()) / (24*60*60*1000)}晚
+                                    </Col>
                                 </Row>
                                 <Row form>
                                     <Col md={3}>
                                         <FormGroup>
                                             <Label for="exampleEmail">姓氏(英文)</Label>
                                             <Input valid={this.state.lastNameValid} invalid={this.state.lastNameinValid} value={this.state.formLastname} onChange = {(e) => this.getLastName(e)}/>
-                                            <FormFeedback valid={this.state.lastNameValid}>必須為英文</FormFeedback>
+                                            <FormFeedback invalid={this.state.lastNameinValid}>必須為英文</FormFeedback>
                                         </FormGroup>
                                     </Col>
                                     <Col md={6}>
                                         <FormGroup>
                                             <Label for="examplePassword">姓名(英文)</Label>
                                             <Input valid={this.state.fisrtNameValid} invalid={this.state.fisrtNameinValid} value={this.state.formFirstname} onChange = {(e) => this.getFirstName(e)}/>
-                                            <FormFeedback valid={this.state.fisrtNameValid}>必填，格式為英文</FormFeedback>
+                                            <FormFeedback invalid={this.state.fisrtNameinValid}>必填，格式為英文</FormFeedback>
                                         </FormGroup>
                                     </Col>
                                     <Col md={3}>
@@ -294,19 +316,19 @@ export default class reservationDetail extends Component {
                                
                                 <FormGroup>
                                     <Label for="exampleEmail">連絡電話</Label>
-                                    <Input  value={this.state.formTel} onChange = {(e) => this.getTel(e)}/>
-                                    {/* <FormFeedback valid>Sweet! that name is available</FormFeedback> */}
+                                    <Input  valid={this.state.telValid} invalid={this.state.telinValid} value={this.state.formTel} onChange = {(e) => this.getTel(e)}/>
+                                    <FormFeedback invalid={this.state.telinValid}>手機格式錯誤(ex:0912345678)</FormFeedback>
                                 </FormGroup>
                                 <FormGroup>
                                     <Label for="examplePassword">電子信箱</Label>
                                     <Input valid={this.state.emailValid} invalid={this.state.emailinValid} value={this.state.formEmail} onChange = {(e) => this.getEmail(e)}/>
-                                    <FormFeedback valid={this.state.emailValid}>信箱格式錯誤，包含＠不得已特殊字元為最後一字元</FormFeedback>
+                                    <FormFeedback invalid={this.state.emailinValid}>信箱格式錯誤，包含＠不得已特殊字元為最後一字元</FormFeedback>
                                     
                                 </FormGroup>
                                 <FormGroup>
                                     <Label for="examplePassword">確認信箱</Label>
-                                    <Input value={this.state.formCheckEmail} onChange = {(e) => this.getCheckEmail(e)} />
-                                    {/* <FormFeedback valid={false}>Oh noes! that name is already taken</FormFeedback> */}
+                                    <Input valid={this.state.checkEmailValid} invalid={this.state.checkEmailinValid} value={this.state.formCheckEmail} onChange = {(e) => this.getCheckEmail(e)} />
+                                    <FormFeedback invalid={this.state.checkEmailinValid}>與上列不相符</FormFeedback>
                                 </FormGroup>
                                 <Row className="extraService">額外加價服務</Row>
                                 <FormGroup className="radioGroup"　tag="fieldset">
@@ -321,7 +343,7 @@ export default class reservationDetail extends Component {
                                         </Label>
                                     </FormGroup>
                                 </FormGroup>
-                                <Link className="linkButton" to="">
+                                <Link className="linkButton">
                                     <Button disabled={false} onClick={this.submitReservation}>確認訂房</Button>
                                 </Link>
                             </Form>
